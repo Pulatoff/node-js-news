fe09ada776662a56bb6a4b951f04c7b15764ba4b
Pulatoff <niyozbekpulatov@gmail.com>
2022-07-12 13:56:02 +0500

    project finished

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const newsRouter = require("./src/routes/news");
 const port = 8000;
+const bodyParser = require("body-parser");
 const APIkey = "ee2381d8fce84333a35e732b945c8584";
 // Static files
 
 app.use(express.static("public"));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", newsRouter);
 
--- src/routes/news.js
+++ src/routes/news.js
@@ -3,10 +3,34 @@ const newsRouter = express.Router();
 const axios = require("axios");
 newsRouter.get("/", async (req, res) => {
   try {
+    const newsAPI = await axios.get(`https://raddy.dev/wp-json/wp/v2/posts/`);
+
+    res.render("news", { articles: newsAPI.data });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+newsRouter.get("/article/:id", async (req, res) => {
+  let articleId = req.params.id;
+  try {
     const newsAPI = await axios.get(
-      `https://newsapi.org/v2/everything?q=tesla&from=2022-06-12&sortBy=publishedAt&apiKey=ee2381d8fce84333a35e732b945c8584`
+      `https://raddy.dev/wp-json/wp/v2/posts/${articleId}`
     );
-    res.render("news", { articles: newsAPI.data.articles });
+
+    res.render("newsSingle", { article: newsAPI.data });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+newsRouter.post("/", async (req, res) => {
+  let articleId = req.body.search;
+  try {
+    const newsAPI = await axios.get(
+      `https://raddy.dev/wp-json/wp/v2/posts?search=${articleId}`
+    );
+    res.render("newsSearch", { articles: newsAPI.data });
   } catch (error) {
     console.log(error.message);
   }
